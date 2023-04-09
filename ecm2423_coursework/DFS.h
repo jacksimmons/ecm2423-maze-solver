afc7d99b4a5dee9646b8f8e3020436d1a6081b33
Jack Simmons <56202789+jacksimmons@users.noreply.github.com>
2023-04-09 20:13:24 +0100

    Delete ecm2423_coursework directory
    deleted an accidental duplicate directory

--- ecm2423_coursework/DFS.h
+++
@@ -1,9 +0,0 @@
-#include <vector>
-#include <tuple>
-
-#include "Vector2.h"
-#include "Maze.h"
-
-// Function templates
-void runDFS();
-Path dfs(Vector2 *start, Vector2 *goal, char* maze, bool* visited);
